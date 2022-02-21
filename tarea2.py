747e7651570d15e6bf2afb8c8be3b3d13ed0081f
fatbatman2 <100149988+fatbatman2@users.noreply.github.com>
2022-02-21 11:07:29 -0500

    Add files via upload

---
+++ tarea2.py
@@ -0,0 +1,7 @@
+numero = float(input("ingrese un numero"))
+
+if numero > 100:
+    print("el numero es mayor que 100")
+    
+else:
+    print("el numero es menor que 100")
---
+++ tarea3.py
@@ -0,0 +1,16 @@
+# hacer programa que compare 2 numeros y me diga si son iguales
+# o cual es mayor 
+
+num_1= float(input("ingrese un numero: "))
+num_2= float(input("ingrese otro numero: "))
+
+if num_1 == num_2:
+    print("son iguales")
+    
+    
+elif num_1 >= num_2:
+    print("el mayor es", num_1)
+    
+    
+elif num_1 <= num_2:
+    print("el mayor es", num_2)
